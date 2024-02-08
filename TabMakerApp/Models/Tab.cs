fa4d92a4425ed8f5bba5d6a7a09c98ae8678dda2
JyeTremlett <58056361+JyeTremlett@users.noreply.github.com>
2024-02-08 13:45:23 +0800

    Created Tab Model

---
+++ TabMakerApp/Models/Tab.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TabMakerApp.Models
+{
+    public class Tab
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? TabContent { get; set; }
+        public string? Description { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set; } = DateTime.Now;
+    }
+}
